a980dd886b00c322d46dc8709630a59efed1f593
ahmedTahha888 <ahmedelmoghany888@gmail.com>
2026-04-30 22:55:43 +0300

    fix loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import { FaSpinner } from 'react-icons/fa6'
+import React from "react";
+import { FaSpinner } from "react-icons/fa6";
 
 export default function loading() {
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <FaSpinner className='animate-spin text-5xl text-global '/>
-   
+    <div className="fixed inset-0 flex justify-center items-center bg-black/20 z-50">
+      <FaSpinner className="animate-spin text-5xl text-global" />
     </div>
-  )
+  );
 }
